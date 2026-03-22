1181d8200ed53819f50b3ef3e6b6f95b457398ad
bluem-kim <labilabikim13@gmail.com>
2026-03-22 23:46:43 +0800

    changes

--- android/app/src/main/java/com/emkim/pageturner/MainActivity.kt
+++ android/app/src/main/java/com/emkim/pageturner/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.pageturner
+package com.emkim.pageturner
 
 import android.os.Build
 import android.os.Bundle
--- android/app/src/main/java/com/emkim/pageturner/MainApplication.kt
+++ android/app/src/main/java/com/emkim/pageturner/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.pageturner
+package com.emkim.pageturner
 
 import android.app.Application
 import android.content.res.Configuration
